5e35cadcd20c301b8504e783a5eb72219c922f52
msdario12 <mansilladario94@gmail.com>
2023-07-05 18:16:31 -0300

    First model of card in home

--- src/main/components/MainCarousel.jsx
+++ src/main/components/MainCarousel.jsx
@@ -1,9 +1,24 @@
-import { Carousel } from 'flowbite-react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { MainCard } from './MainCard';
 
 export const MainCarousel = () => {
+	const settings = {
+		dots: true,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+	};
 	return (
-		<div className='min-h-screen'>
+		<div>
 			<h2>Carousel</h2>
+			<div>
+				<Slider {...settings}>
+					<MainCard className={'mx-5 my-3 object-cover'} />
+				</Slider>
+			</div>
 		</div>
 	);
 };
