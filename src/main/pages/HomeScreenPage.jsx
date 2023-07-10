b3f6ee96a9c69cd807c26819c2f5d6fed40f53f4
msdario12 <mansilladario94@gmail.com>
2023-07-10 17:15:37 -0300

    Moved flowbite and footer component to MainRoutes

--- src/main/pages/HomeScreenPage.jsx
+++ src/main/pages/HomeScreenPage.jsx
@@ -1,32 +1,24 @@
-import { Flowbite } from 'flowbite-react';
 import { MainCarousel } from '../components/MainCarousel';
 import { MainSearch } from '../components/MainSearch';
 
 import { FeatureSection } from '../components/FeatureSection';
-import { MainFooter } from '../../footer/components/MainFooter';
 
 export const HomeScreenPage = () => {
 	return (
-		<Flowbite>
-
-			<div className='lg:container mx-auto px-4'>
-				<main>
-					<section className='h-96 grid place-content-center '>
-						<h1 className='text-5xl text-center tracking-tight font-extrabold'>
-							Busca tus locales favoritos y reserva un turno fácilmente!
-						</h1>
-						<MainSearch className='my-10 flex flex-col gap-4' />
-					</section>
-					<section>
-						<h2 className='text-3xl font-bold tracking-tighter'>Recomendado</h2>
-						<MainCarousel />
-					</section>
-					<FeatureSection />
-				</main>
-			</div>
-			<footer>
-				<MainFooter />
-			</footer>
-		</Flowbite>
+		<div className='lg:container mx-auto px-4'>
+			<main>
+				<section className='h-96 grid place-content-center '>
+					<h1 className='text-5xl text-center tracking-tight font-extrabold'>
+						Busca tus locales favoritos y reserva un turno fácilmente!
+					</h1>
+					<MainSearch className='my-10 flex flex-col gap-4' />
+				</section>
+				<section>
+					<h2 className='text-3xl font-bold tracking-tighter'>Recomendado</h2>
+					<MainCarousel />
+				</section>
+				<FeatureSection />
+			</main>
+		</div>
 	);
 };
--- src/main/routes/MainRoutes.jsx
+++ src/main/routes/MainRoutes.jsx
@@ -1,28 +1,32 @@
-import { MainNavbar } from "../../nav/components/MainNavbar"
-import { Route, Routes } from "react-router-dom"
-import { DashBoardAdminPage } from "../pages/DashBoardAdminPage"
-import { DashBoardUserPage } from "../pages/DashBoardUserPage"
-import { TurnsReservationPage } from "../pages/TurnsReservationPage"
-import { HomeScreenPage } from "../pages/HomeScreenPage"
-
-
-
+import { MainNavbar } from '../../nav/components/MainNavbar';
+import { Route, Routes } from 'react-router-dom';
+import { DashBoardAdminPage } from '../pages/DashBoardAdminPage';
+import { DashBoardUserPage } from '../pages/DashBoardUserPage';
+import { TurnsReservationPage } from '../pages/TurnsReservationPage';
+import { HomeScreenPage } from '../pages/HomeScreenPage';
+import { Flowbite } from 'flowbite-react';
+import { MainFooter } from '../../footer/components/MainFooter';
 
 export const MainRoutes = () => {
-    return (
-        <>
-            <div className='lg:container mx-auto px-4'>
+	return (
+		<>
+			<div className='lg:container mx-auto px-4'>
+				<Flowbite>
+					<div>
+						<MainNavbar />
+					</div>
 
-                <MainNavbar />
-
-                <Routes>
-                    <Route path="/dashadmin" element={<DashBoardAdminPage />} />
-                    <Route path="/dashuser" element={<DashBoardUserPage />} />
-                    <Route path="/turns" element={<TurnsReservationPage />} />
-                    <Route path="/" element={<HomeScreenPage />} />
-                </Routes>
-
-            </div>
-        </>
-    )
-}
+					<Routes>
+						<Route path='/dashadmin' element={<DashBoardAdminPage />} />
+						<Route path='/dashuser' element={<DashBoardUserPage />} />
+						<Route path='/turns' element={<TurnsReservationPage />} />
+						<Route path='/' element={<HomeScreenPage />} />
+					</Routes>
+					<footer>
+						<MainFooter />
+					</footer>
+				</Flowbite>
+			</div>
+		</>
+	);
+};
